af007d7e207087dc7194bdf5e8ed4b6e9258cf32
An Madridstar <internship>
2024-03-11 11:08:44 +0700

    fix error message value JS

--- js/index.js
+++ js/index.js
@@ -12,38 +12,20 @@ labels.forEach((label) => {
     .join("");
 });
 
-function toggleActiveClass(label, isActive) {
-  if (isActive) {
-    label.classList.add("active");
+usernameInput.addEventListener("input", () => {
+  if (usernameInput.value === "") {
+    errorSpan1.textContent = "Vui lòng nhập tên đăng nhập!";
   } else {
-    label.classList.remove("active");
+    errorSpan1.textContent = "";
   }
-}
-
-function handleInputFocus(input, label) {
-  input.classList.add("focused");
-  toggleActiveClass(label, true);
-}
-
-function handleInputBlur(input, label) {
-  input.classList.remove("focused");
-  toggleActiveClass(label, input.value.trim() !== "");
-}
-
-usernameInput.addEventListener("focus", () => {
-  handleInputFocus(usernameInput, labels[0]);
 });
 
-usernameInput.addEventListener("blur", () => {
-  handleInputBlur(usernameInput, labels[0]);
-});
-
-passInput.addEventListener("focus", () => {
-  handleInputFocus(passInput, labels[1]);
-});
-
-passInput.addEventListener("blur", () => {
-  handleInputBlur(passInput, labels[1]);
+passInput.addEventListener("input", () => {
+  if (passInput.value === "") {
+    errorSpan2.textContent = "Vui lòng nhập mật khẩu!";
+  } else {
+    errorSpan2.textContent = "";
+  }
 });
 
 form.addEventListener("submit", (e) => {
@@ -54,15 +36,11 @@ form.addEventListener("submit", (e) => {
   if (usernameInput.value === "") {
     errorSpan1.textContent = "Vui lòng nhập tên đăng nhập!";
     hasError = true;
-  } else {
-    errorSpan1.textContent = "";
   }
 
   if (passInput.value === "") {
     errorSpan2.textContent = "Vui lòng nhập mật khẩu!";
     hasError = true;
-  } else {
-    errorSpan2.textContent = "";
   }
 
   if (!hasError) {
